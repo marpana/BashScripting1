563382dfe5ca7311250dc714d4926670ad565a6f
ArpanaM <muriki.arpana@gmail.com>
2024-11-06 10:32:41 +0530

    new file created

--- scripts.sh
+++ scripts.sh
@@ -4,14 +4,14 @@ read -p "enter your age: " age
 echo $age
 
 # -z is used to check whether a user entered a value or not
-if [ -z "$age" ]; then
-  echo Input missing
+#if [ -z "$age" ]; then
+#  echo Input missing
 
-  elif [ "${age}" -lt 18 ]; then
+  if [ ! -z "${age}" -a "${age}" -lt 18 ]; then
   echo you are minor
-  elif [ "${age}" -gt 60 ]; then
+  elif [ ! -z "${age}" -a "${age}" -gt 60 ]; then
     echo you are senior citizen
   else
     echo you are major
 
-fi
+  fi
