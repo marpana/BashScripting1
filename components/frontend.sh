d36ae9c9abce1fc2d21b50c6649939c4fc92f2a0
ArpanaM <muriki.arpana@gmail.com>
2024-11-06 10:57:10 +0530

    new file created

--- components/frontend.sh
+++ components/frontend.sh
@@ -3,3 +3,16 @@
 # check whether the script is running as a root user or not
 
   echo frontend setup...
+ yum install nginx -y
+ systemctl enable nginx
+ systemctl start nginx
+
+# curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+#
+#  cd /usr/share/nginx/html
+#  rm -rf *
+#  unzip /tmp/frontend.zip
+#  mv frontend-main/* .
+#  mv static/* .
+#  rm -rf frontend-master static README.md
+#  mv localhost.conf /etc/nginx/default.d/roboshop.conf
